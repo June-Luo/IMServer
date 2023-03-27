98ef7b1097ae5c470bf874bda3dc3452a5f2858b
laibaijiang <13198222888@163.com>
2023-03-27 21:36:11 +0800

    message entity commit

--- src/main/java/com/mengxiang/im/push/bean/db/Message.java
+++ src/main/java/com/mengxiang/im/push/bean/db/Message.java
@@ -41,9 +41,24 @@ public class Message {
     @Column(nullable = false)
     private int type;
 
+    //发送者
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "senderId")
+    //多个消息对应一个发送者(一个人可以发送很多消息)
     private User sender;
 
+    //该字段为了对应sender的数据库字段的senderId，不允许手动更新或者插入
+    @Column(updatable = false, insertable = false)
+    private String senderId;
+
+    //接受者
+    //多个消息对应一个接受者
+    @ManyToOne
+    @JoinColumn(name = "receiverId")
     private User receiver;
+
+    @Column(updatable = false, insertable = false)
+    private String receiverId;
 
     //定义创建时间戳，在创建的时候就写入
     @CreationTimestamp
@@ -54,4 +69,84 @@ public class Message {
     @CreationTimestamp
     @Column(nullable = false)
     private LocalDateTime udateAt = LocalDateTime.now();
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getAttach() {
+        return attach;
+    }
+
+    public void setAttach(String attach) {
+        this.attach = attach;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public User getSender() {
+        return sender;
+    }
+
+    public void setSender(User sender) {
+        this.sender = sender;
+    }
+
+    public String getSenderId() {
+        return senderId;
+    }
+
+    public void setSenderId(String senderId) {
+        this.senderId = senderId;
+    }
+
+    public User getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(User receiver) {
+        this.receiver = receiver;
+    }
+
+    public String getReceiverId() {
+        return receiverId;
+    }
+
+    public void setReceiverId(String receiverId) {
+        this.receiverId = receiverId;
+    }
+
+    public LocalDateTime getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(LocalDateTime createAt) {
+        this.createAt = createAt;
+    }
+
+    public LocalDateTime getUdateAt() {
+        return udateAt;
+    }
+
+    public void setUdateAt(LocalDateTime udateAt) {
+        this.udateAt = udateAt;
+    }
 }
